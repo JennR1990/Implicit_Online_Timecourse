c79b521a4c60a7d937631e461c3f61ad61a76933
Jenn Ruttle <jennruttle@gmail.com>
2022-05-19 13:09:14 -0400

    consolidated plot codes to one script

--- R/clean&plot.R
+++ R/clean&plot.R
@@ -1,3 +1,40 @@
+plotschedule<- function(){
+  
+  
+  plot(NULL, xlab = "Trials", ylab = "Perturbation", main = "Rotation Schedule", ylim = c(-45,45), xlim = c(0,144), axes = FALSE, cex.lab = 1.25)
+  lines(c(1, 20, 20, 120, 120, 128, 128), c(0, 0, 45, 45, -45, -45, 0), col = rgb(0., 0., 0.))
+  lines(c(128, 144), c(0, 0), lty = 2, col = rgb(0., 0., 0.))
+  axis(2, at = c(-45, -25, 0, 25, 45), cex.axis = 1.25,
+       las = 2)
+  axis(1, at = c(1, 20, 120, 128, 144), cex.axis = 1.25, las = 2)
+}
+
+
+plotdata <- function(filename, cond, task, fun){
+  data <- read.csv(filename, header = TRUE)
+  data <- removeReachOutliers(data)
+  method <- as.character(fun)
+  title <- sprintf("%s Deviations for Condition %d Using %s", task, cond, method)
+  plot(apply(data[,2:ncol(data)], 1, fun, na.rm=T), type = 'l', xlab = "Trials", ylab = "Deviations", main = title, ylim = c(-45,45), col = "Blue", axes = FALSE, cex.lab = 1.25)
+  points(x = rep(25, times = ncol(data) - 1), y = data[25, 2:ncol(data)])
+  points(x = rep(123, times = ncol(data) - 1), y = data[123, 2:ncol(data)])
+  lines(c(1, 25, 25, 125, 125, 133, 133), c(0, 0, -45, -45, 45, 45, 0), col = rgb(0., 0., 0.))
+  lines(c(133, 148), c(0, 0), lty = 2, col = rgb(0., 0., 0.))
+  
+  legend(
+    5,
+    40,
+    legend = task,
+    col = "Blue",
+    lty = c(1),
+    lwd = c(2),
+    bty = 'n',
+    cex = 1.25
+  )
+  axis(2, at = c(-45, -25, 0, 25, 45), cex.axis = 1.25,
+       las = 2)
+  axis(1, at = c(1, 25, 125, 133, 148), cex.axis = 1.25, las = 2)
+}
 
 
 plotindividualcurves<- function() {
@@ -291,33 +328,5 @@ t.interval = function(data, variance = var(data, na.rm = TRUE), conf.level = 0.9
   
   return(c(xbar - z * sdx, xbar + z * sdx))
   
-}
-
-
-Cleandata<- function (filename){
-data<-read.csv(filename, header = TRUE)
-
-aligned<-data[data$rot =="baseline",2:ncol(data)]
-rot1<-data[data$rot =="rotation",2:ncol(data)]
-counter<-data[data$rot =="counter",2:ncol(data)]
-errorclamp<-data[data$rot =="errorclamp",2:ncol(data)]
-
-
-
-aligned[aligned > 40] <- NA
-aligned[aligned < -40] <- NA
-
-rot1[rot1 < -75]<- NA
-rot1[rot1 >  15]<- NA
-
-counter[counter < -30]<- NA
-counter[counter >  75]<- NA
-
-errorclamp[errorclamp > 40] <- NA
-errorclamp[errorclamp < -40] <- NA
-
-
-alldata<- rbind(aligned,rot1,counter,errorclamp)
-return(alldata)
 }
 
