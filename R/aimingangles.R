5451ac6053b121dd0d37014c9bf3e607874358d5
Jenn Ruttle <jennruttle@gmail.com>
2022-05-19 16:12:39 -0400

    small update to aiming code

--- R/aimingangles.R
+++ R/aimingangles.R
@@ -19,11 +19,11 @@ setwd("E:/Jenn/Documents/Implicit_Online_Timecourse/data")
      aiming_directions<- rbind(aiming_directions,df$aiming_direction[df$aiming_type == 1])
 
      
-     finalaiming<-data.frame(matrix(NA, nrow = 15,ncol = 4))
+     finalaiming<-data.frame(matrix(NA, nrow = length(files),ncol = 4))
      names(finalaiming)<- c("45", "90", "90", "135")
      targetlocs<- c(45,90,90,135)
     
-     for (i in 1:15){
+     for (i in 1:length(files)){
        for (target in c(1,4)){
          indx<-which(aiming_targets[i,] == targetlocs[target])
          finalaiming[i,target]<-aiming_directions[i,indx] 
@@ -32,7 +32,7 @@ setwd("E:/Jenn/Documents/Implicit_Online_Timecourse/data")
      }
      
      
-     for (i in 1:15){
+     for (i in 1:length(files)){
        indx<-which(aiming_targets[i,] == targetlocs[3])
        finalaiming[i,2:3]<-aiming_directions[i,indx] 
      }
