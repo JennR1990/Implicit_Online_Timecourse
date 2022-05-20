dc2d0499325e9eeda26348b1a5b08cc97602e339
Jenn Ruttle <jennruttle@gmail.com>
2022-05-19 20:22:16 -0400

    updated aiming angle to create a file for each condition

--- R/aimingangles.R
+++ R/aimingangles.R
@@ -23,30 +23,38 @@ setwd("E:/Jenn/Documents/Implicit_Online_Timecourse/data")
      names(finalaiming)<- c("45", "90", "90", "135")
      targetlocs<- c(45,90,90,135)
     
-     for (i in 1:length(files)){
-       for (target in c(1,4)){
-         indx<-which(aiming_targets[i,] == targetlocs[target])
-         finalaiming[i,target]<-aiming_directions[i,indx] 
-       }
-       
-     }
-     
-     
-     for (i in 1:length(files)){
-       indx<-which(aiming_targets[i,] == targetlocs[3])
-       finalaiming[i,2:3]<-aiming_directions[i,indx] 
-     }
-     
-     
-     
-     
-   }
 
    
+   
+   }
+   for (i in 1:length(files)){
+     for (target in c(1,4)){
+       indx<-which(aiming_targets[i,] == targetlocs[target])
+       finalaiming[i,target]<-aiming_directions[i,indx] 
+     }
+     
+   }
+   
+   
+   for (i in 1:length(files)){
+     indx<-which(aiming_targets[i,] == targetlocs[3])
+     finalaiming[i,2:3]<-aiming_directions[i,indx] 
+   }
+   
+   output<- sprintf("../AimingAngle/%d_aimingangle.csv", group)
+write.csv(finalaiming, output, quote = FALSE, row.names = FALSE)
+   
  }
- 
-return(finalaiming)
+
 }
+ 
+
+
+ 
+
+ 
+ 
+ 
  
 
 
